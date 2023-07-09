1b3a5f3b0ffc12e1497085ab3bfe8e4bc63ee55b
nolood <a9514548799@gmail.com>
2023-07-09 13:27:19 +0500

    Начало сервиса

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // constructor(@InjectModel(User) private userRepository: typeof User) {}
+  // async createUser(dto: CreateUserDto) {}
+  // async getAllUsers() {}
+}
